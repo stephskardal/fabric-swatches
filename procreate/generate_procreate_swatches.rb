35cdab8e7aea51df8fab104ceab3dc85abb55e4a
Steph Skardal <stephskardal@Stephs-MacBook-Air.local>
2021-12-04 21:00:32 -0500

    Doing some things.

--- procreate/generate_procreate_swatches.rb
+++ procreate/generate_procreate_swatches.rb
@@ -1,5 +1,7 @@
-require 'procreate/swatches'
-require 'zip'
+#!/usr/bin/ruby
+
+gem 'activesupport'
+require './mod_procreate/swatches'
 
 f = open('../fabricSwatches.js', 'r')
 swatch_data = eval(f.read.gsub(/\n/, '').gsub(/var /, ''))
@@ -7,14 +9,13 @@ swatch_data = eval(f.read.gsub(/\n/, '').gsub(/var /, ''))
 swatch_data.each do |key, fabric_line|
   zipfile_name = "#{key}.zip"
   swatches_filenames = []
-  fabric_line[:swatches].values.each_slice(30).each_with_index do |set, index|
-    swatches_filenames << Procreate::Swatches.export("#{key}-#{index}", set)
+  swatches_filenames = fabric_line[:swatches].each_slice(30).map.with_index do |set, index|
+    Procreate::Swatches.export("#{key}-#{index}", set)
   end
-  Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-    swatches_filenames.each do |filename|
-      zipfile.add(File.basename(filename), filename)
-    end
+  exec 'mkdir #{key}'
+  swatches_filenames.each do |filename|
+    exec 'mv #{key}* #{key}'
   end
 end
 
-exec 'rm *.swatches'
+exec 'rm ./*.swatches'
