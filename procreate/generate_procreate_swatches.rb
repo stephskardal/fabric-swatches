116f4e7b00bee5f6475792f460d31201855fd543
Steph Skardal <stephskardal@gmail.com>
2024-02-21 11:12:17 -0500

    Cleanup / etc

--- procreate/generate_procreate_swatches.rb
+++ procreate/generate_procreate_swatches.rb
@@ -5,20 +5,24 @@ require './mod_procreate/swatches'
 
 f = open('../outputs/fabricSwatches.js', 'r')
 swatch_data = eval(f.read.gsub(/\n/, '').gsub(/export const fabricSwatches = /, ''))
+
+PROCREATE_SWATCH_SIZE = 30.freeze
 ILLUSTRATOR_SWATCH_SIZE = 32.freeze
 
 swatch_data.each do |key, fabric_line|
   next if key == :pantone
 
-  fabric_line[:swatches].each_slice(30).map.with_index do |set, index|
-    name = "#{key.capitalize} Set ##{index + 1}"
-    Procreate::Swatches.export(name, set, { export_directory: key.to_s, file_name: "#{key}-#{index + 1}" })
-  end
-
-  # fabric_line[:swatches].each_slice(ILLUSTRATOR_SWATCH_SIZE).map.with_index do |set, index|
+  # TODO: Remove */*.swatches 
+  # fabric_line[:swatches].each_slice(PROCREATE_SWATCH_SIZE).map.with_index do |set, index|
   #   name = "#{key.capitalize} Set ##{index + 1}"
-  #  Procreate::Swatches.export(name, set, { export_directory: "../illustrator/palette-in/", file_name: "#{key}-#{index + 1}" })
+  #   Procreate::Swatches.export(name, set, { export_directory: key.to_s, file_name: "#{key}-#{index + 1}" })
   # end
+
+  # TODO: Remove ../illustrator/palette-in
+  fabric_line[:swatches].each_slice(ILLUSTRATOR_SWATCH_SIZE).map.with_index do |set, index|
+    name = "#{key.capitalize} Set ##{index + 1}"
+   Procreate::Swatches.export(name, set, { export_directory: "../illustrator/palette-in/", file_name: "#{key}-#{index + 1}" })
+  end
 end
   
-p "Did the thing. To generate illustrator files, copy *.swatches to illustrator/palette-in/"
+p "Did the thing."
